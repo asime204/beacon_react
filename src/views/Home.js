5e8da4d0ccbcd0354731c011b87664d6c175f6d2
asime204 <azielsime1@gmail.com>
2023-03-09 11:35:15 -0500

    final commit

--- src/views/Home.js
+++ src/views/Home.js
@@ -15,7 +15,7 @@ function Home(props) {
   return (
     <div className='body'>
       <h1 className={`fade-in ${textLoaded ? 'loaded' : ''}`}>Welcome to Beacon</h1>
-      <h3 className={`fade-in ${textLoaded ? 'loaded' : ''}`}>Your personal budget quickshot app</h3>
+      <h3 className={`fade-in ${textLoaded ? 'loaded' : ''}`}>Your personal budget snapshot app</h3>
       <p className={`fade-in ${textLoaded ? 'loaded' : ''}`}>Get started by creating an account or logging in to your existing account.</p>
       <div className="card">
         <div className={`card-body ${buttonsLoaded ? 'loaded' : ''}`}>
