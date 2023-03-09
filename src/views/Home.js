26a464ee68b0d78cff74dabbb5f6e784371ad261
asime204 <azielsime1@gmail.com>
2023-03-09 07:25:58 -0500

    color scheme home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -9,12 +9,12 @@ function Home(props) {
     return (
         <div className='body'>
             <h1>Welcome to Beacon</h1>
-            <h3>Your personal financial tracking app</h3>
+            <h3>Your personal budget quickshot app</h3>
             <p>Get started by creating an account or logging in to your existing account.</p>
             <div className="card">
                 <div className="card-body">
-                    <button className="button"><a href="/signup">Create Account</a></button>
-                    <button className="button"><a href="/login">Log In</a></button>
+                    <button className="homebutton"><a href="/signup">Create Account</a></button>
+                    <button className="homebutton"><a href="/login">Log In</a></button>
                     {/* <button onClick={createPopup}>Log In with Google</button> */}
                 </div>
             </div>
